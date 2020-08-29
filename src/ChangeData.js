7a63c5c0977c61c25be0e7713e82678f8f55d803
nagimemooo <nagimemooo@gmail.com>
2020-08-29 16:45:44 +0800

    FixL remove class version

--- src/ChangeData.js
+++ src/ChangeData.js
@@ -4,45 +4,20 @@ import React, { useState, useEffect } from "react";
 const ChangeData = props => {
   const [Data, setData] = useState("初始資料");
   const [isRightData, setRightData] = useState(true);
-  // class ChangeData extends Component {
-  //   constructor(props) {
-  //     super(props);
-  //     this.state = {
-  //       isRightData: true,
-  //       Data: "初始資料"
-  //     };
-  //     this.handleClick = this.handleClick.bind(this);
-  //   }
   const handleClick = () => {
-    //handleClick() {
-    //this.setState({ isRightData: !this.state.isRightData });
+
     setRightData(!isRightData);
   };
-  // //判斷props或state是否被更動
-  //   componentDidUpdate(prevProps, prevState, snapshot) {
-  //   //如果不加if條件的話react limits the number of nested updates to prevent infinite loops
-  //     if (this.props.detail === "B" && prevProps.detail !== this.props.detail) {
-  //       this.setState({ Data: "B" });
-  //     }
-  //     if (prevState.isRightData !== this.state.isRightData) {
-  //       this.setState({ Data: "isRightData被更改" });
-  //     }
-  //   }
+  
   useEffect(() => {
-    /*  componentDidMount和componentDidUpdate */
     if (props.detail === "B") setData("B");
   }, [props.detail]);
 
   useEffect(() => {
-    /*  componentDidMount和componentDidUpdate */
     setData("isRightData被更改");
   }, [isRightData]);
-  //render() {
   return (
-    // 資料可以被改變的內容區塊:{this.state.Data}
-    // {this.state.isRightData === true
-    //   ? "點我更改state(isRightData)"
-    //   : "重新出現"}
+ 
     <div className="c-chgData" id="msg">
       資料可以被改變的內容區塊:{Data}
       <div>
@@ -53,6 +28,6 @@ const ChangeData = props => {
       </div>
     </div>
   );
-  // }
+  
 };
 export default ChangeData;
--- src/Data.js
+++ src/Data.js
@@ -1,37 +1,10 @@
-//import React, { Component } from "react";
-//import React, { useState } from 'react';
 import React, { useState, useEffect } from "react";
 //拿資料及移除的範例
-//class Data extends Component {
+
   const Data = props => {  
     const [isGetData, setGetData] = useState(false);
     const [delayData, setDelayData] = useState("");
-// constructor(props) {
-//     super(props);
-//     this.state = {
-//       isGetData: false,
-//       delayData: ""
-//     };
-//     this.ajaxSimulator = this.ajaxSimulator.bind(this);
-//   }
 
-
-
-  // //此data元件被呼叫就會做了
-  // componentDidMount() {
-  //   this.ajaxSimulator();
-  //   document
-  //     .getElementById("textArea")
-  //     .append("[id:textArea]我是在Mount時新增出現在APP裡的區塊!");
-  //   var node = document.createElement("LI");
-  //   var textnode = document.createTextNode("Water list");
-  //   node.appendChild(textnode);
-  //   document.getElementById("textArea").appendChild(node);
-  // }
-  // //此data元件被移除就會做了
-  // componentWillUnmount() {
-  //   document.getElementById("textArea").innerHTML = "";
-  // }
   useEffect(() => {
     /* componentDidMount 區*/
     ajaxSimulator();
@@ -50,32 +23,26 @@ import React, { useState, useEffect } from "react";
   }, []);
 
 
-  // ajaxSimulator() {
+
     const ajaxSimulator = () => {
       setTimeout(() => {
         setGetData(true);
         setDelayData("3秒後拿到的資料");
-  
-        // this.setState({ isGetData: true, Mom: "3秒後拿到的資料" });
       }, 3000);
     };
   
 
-  //render() {
-  // if (this.state.isGetData === false)
   if (isGetData === false)
     return (
       <div className="content-bady" id="msg">
         讀取中...
       </div>
     );
-  //  讀取完了 資料是{this.state.Mom}
   else
     return (
       <div className="content-bady" id="msg">
         讀取完了 資料是{delayData}
       </div>
     );
-  //}
 };
 export default Data;
