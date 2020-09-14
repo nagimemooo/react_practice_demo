3ea4dbb5cbdff88da6298e9247b0440be6e4e8a1
nagimemooo <nagimemooo@gmail.com>
2020-09-14 22:49:49 +0800

    Add: input style

--- src/component/Suggest.js
+++ src/component/Suggest.js
@@ -1,8 +1,38 @@
 
 import React , { useRef }from 'react';
-
+import { makeStyles } from "@material-ui/core/styles";
+const useStyles = makeStyles((theme) => ({
+  input: {
+    borderRadius: 4,
+    position: 'relative',
+    backgroundColor: theme.palette.background.paper,
+    border: '1px solid #ced4da',
+    fontSize: 16,
+    padding: '10px 6px 10px 6px',
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:focus': {
+      borderRadius: 4,
+      borderColor: '#80bdff',
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+    },
+  }
+}));
 
   const Suggest=(props)=>{
+    const classes = useStyles();
     // getValue(){
     //   return this.refs.lowlevelinput.valaue;
     // }
@@ -19,6 +49,7 @@ import React , { useRef }from 'react';
            defaultValue={props.defaultValue}
            ref={inputRef}
            id={props.id}
+           className={classes.input}
            />  
           <datalist id={randomid} >
           {props.options.map((item,idx)=>
@@ -30,7 +61,7 @@ import React , { useRef }from 'react';
 
   }
   
-export default SuggestOpt;
+export default Suggest;
 //1.class Suggest extends React.Component{
 //改成const SuggestOpt=(props)=>{
 //2.拿掉render(){}
