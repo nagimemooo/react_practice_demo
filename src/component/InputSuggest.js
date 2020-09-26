710d5eced8ea1f196d68aa88d02e57d4d04bc974
nagimemooo <nagimemooo@gmail.com>
2020-09-26 22:01:19 +0800

    Fix: label onfocus to move position

--- src/component/InputSuggest.js
+++ src/component/InputSuggest.js
@@ -1,6 +1,10 @@
 import React, { useRef, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-const useStyles = makeStyles(theme => ({
+
+import { myStyle } from './Style/InputSuggest_Style';
+/** @jsx jsx */ import { css, jsx } from '@emotion/core';
+
+const useStyles = makeStyles((theme) => ({
   input: {
     borderRadius: 4,
     position: 'relative',
@@ -10,34 +14,57 @@ const useStyles = makeStyles(theme => ({
     padding: '10px 6px 10px 6px',
     transition: theme.transitions.create(['border-color', 'box-shadow']),
     // Use the system font instead of the default Roboto font.
-    fontFamily: ['-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"'].join(','),
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
     '&:focus': {
       borderRadius: 4,
       borderColor: '#80bdff',
-      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)'
-    }
-  }
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+    },
+  },
 }));
 
-const Suggest = props => {
+const Suggest = (props) => {
   const classes = useStyles(); // const handleClick = () => {
   //   inputRef.current.focus(); //滑鼠會跳到input的欄位
   // };
 
-  const handleChangeValue = event => {
+  const handleChangeValue = (event) => {
     console.log(inputRef.current.value);
     props.func(inputRef.current.value);
   };
 
   const inputRef = useRef(null);
   const randomid = Math.random().toString(16).substring(2);
-  return <div>
-      <input list={randomid} // defaultValue={props.defaultValue}
-    ref={inputRef} id={props.id} className={classes.input} onChange={handleChangeValue} />
+  return (
+    <div css={myStyle.FormControl}>
+      <input
+        list={randomid} // defaultValue={props.defaultValue}
+        ref={inputRef}
+        id={props.id}
+        css={[myStyle.Input.base, myStyle.Input.focus]}
+        onChange={handleChangeValue}
+      />
+      <label htmlFor="TaskName" css={myStyle.Lable}>
+        Type
+      </label>
       <datalist id={randomid}>
-        {props.options.map((item, idx) => <option value={item} key={idx} />)}
+        {props.options.map((item, idx) => (
+          <option value={item} key={idx} />
+        ))}
       </datalist>
-    </div>;
+    </div>
+  );
 };
 
 export default Suggest;
